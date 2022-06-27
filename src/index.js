4e0b2e42511c123cfe12bd0de89a972ff37fe497
Tyler Schnabel <tylerwschnabel@gmail.com>
2022-06-27 12:56:52 -0700

    fixed createProject function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import _ from 'lodash';
+import './style.css';
+import ListItem from './modules/todoItems';
+import Project from './modules/project';
+
+
+function createProject(){
+    console.log("CreateProject running");
+    let projectFormName = document.getElementById("add-project-name");
+    let projectFormDescription = document.getElementById("add-project-description");
+    let projectList = document.getElementById('project-list');
+
+    let newProject = new Project(projectFormName.value, projectFormDescription.value);
+
+    let newProjectDiv = document.createElement('div');
+    newProjectDiv.setAttribute('class', "project-name");
+    newProjectDiv.textContent = newProject.title;
+    projectList.appendChild(newProjectDiv);
+    console.log("project added");
+}
+
+function print(){
+    let projectFormName = document.getElementById("add-project-name");
+    console.log (projectFormName);
+}
+    
+let addProjectBtn = document.getElementById("add-project-button");
+addProjectBtn.addEventListener("click", createProject);
---
+++ src/modules/project.js
@@ -0,0 +1,14 @@
+import { CleanPlugin } from "webpack";
+
+class Project {
+    constructor(title, description){
+        this.title = title;
+        this.description = description;
+    }
+
+    addItem(){
+
+    }
+}
+
+export default Project
